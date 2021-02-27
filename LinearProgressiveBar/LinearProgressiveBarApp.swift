6ad5d41ed97d8707bc58cbdf913e86a09a84dddd
Elechim01 <mikymanny015@gmail.com>
2021-02-27 17:42:50 +0100

    Initial Commit

---
+++ LinearProgressiveBar/LinearProgressiveBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinearProgressiveBarApp.swift
+//  LinearProgressiveBar
+//
+//  Created by Michele Manniello on 27/02/21.
+//
+
+import SwiftUI
+
+@main
+struct LinearProgressiveBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
